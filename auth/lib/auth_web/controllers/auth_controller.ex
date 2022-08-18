3a2a9fc17b954ac567bf70c2089d95f1f734e8fb
SHOBHIT YADAV <shobityadav23@gmail.com>
2022-08-18 10:15:50 -0700

    POST req working

--- auth/lib/auth_web/controllers/auth_controller.ex
+++ auth/lib/auth_web/controllers/auth_controller.ex
@@ -1,6 +1,7 @@
 defmodule AuthWeb.AuthController do
   use AuthWeb, :controller
   alias Auth.Accounts
+  alias AuthWeb.Utils
   def ping(conn , _params) do
     conn
     |> render("ack.json" , %{success: true , message: "Pong"})
@@ -9,9 +10,10 @@ defmodule AuthWeb.AuthController do
   def register(conn , params) do
     case Accounts.create_user(params) do
       {:ok , _user} ->  conn |> render("ack.json" , %{success: true , message: "Registration Successful"})
-      {:error , %Ecto.Changeset{} = changeset} -> conn |> render("error.json" , %{errors: Utils.format_changeset_errors(changeset)})
+      {:error , %Ecto.Changeset{} = changeset} -> conn |> render("errors.json" , %{errors: Utils.format_changeset_errors(changeset)})
 
       _ -> conn |>  render("error.json" , %{error: Utils.internal_server_error()})
     end
+
   end
 end
