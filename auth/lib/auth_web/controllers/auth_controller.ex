7faacb9bb834dadf01a7c5dc74ec19df4defd702
SHOBHIT YADAV <shobityadav23@gmail.com>
2022-08-18 02:57:52 -0700

    New Schema Added

--- auth/lib/auth_web/controllers/auth_controller.ex
+++ auth/lib/auth_web/controllers/auth_controller.ex
@@ -1,8 +1,13 @@
 defmodule AuthWeb.AuthController do
   use AuthWeb, :controller
-
+  alias Auth.Accounts
   def ping(conn , _params) do
     conn
     |> render("ack.json" , %{success: true , message: "Pong"})
+  end
+
+  def register(conn , params) do
+    params |> Accounts.create_user()
+    conn |> render("ack.json" , %{success: true , message: "Registration Successful"})
   end
 end
