65f53a6d1050a356a778f651d33662d04e897b34
SHOBHIT YADAV <shobityadav23@gmail.com>
2022-08-26 07:00:31 -0700

    user resolvers added

---
+++ medium_graphql_api/lib/medium_graphql_api_web/resolvers/user_resolver.ex
@@ -0,0 +1,6 @@
+defmodule MediumGraphqlApiWeb.Resolvers.UserResolver do
+  alias MediumGraphqlApi.Accounts
+    def users(_,_,_) do
+        {:ok, Accounts.list_users()}
+    end
+end
--- medium_graphql_api/lib/medium_graphql_api_web/schema.ex
+++ medium_graphql_api/lib/medium_graphql_api_web/schema.ex
@@ -3,7 +3,10 @@ defmodule MediumGraphqlApi.Schema do
     #import types
     import_types(MediumGraphqlApiWeb.Schema.Types)
     query do
-        
+        @desc "Get list of all users"
+        field :users,  list_of(:user_type) do
+        resolve(&Resolver.UserResolver.users/3)
+        end
     end
 
     # mutation do
